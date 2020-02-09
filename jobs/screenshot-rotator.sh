be24abe0c5edf17c5eb2e308d5fa049cbf88c41e
Dawson Mortenson <dawson.mortenson@gmail.com>
2020-02-08 18:49:26 -0800

    add logging and update job path

--- jobs/screenshot-rotator.sh
+++ jobs/screenshot-rotator.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+START=`date +%s`
+echo "############################## $(date -u) ##############################"
+
+# Screenshot Directory Update
 SHOT_PATH="/Users/banthaherder/Documents/screenshots/$(date +'%B-%Y')"
 mkdir -p "$(echo $SHOT_PATH)"
 defaults write com.apple.screencapture location $SHOT_PATH
+
+END=`date +%s`
+RUNTIME=$(($END-$START))
+echo "Default screenshots directory updated to $SHOT_PATH in $RUNTIME seconds"
+echo "##########################################################################################"
